50b4933d538c6c7e5d6f2bdda964fc14f296f1b9
Fred Hatfull <fred.hatfull@gmail.com>
2011-09-07 21:02:03 -0400

    Ignoring pyc and moved to util

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
+from util import fib
+
 def main():
 	max_fib = 30
 	for i in range(0, max_fib):
 		print fib(i)
 
-def fib(n):
-	"""Find the nth term of the Fibonacci sequence."""
-	if n < 2:
-		return n
-	
-	return fib(n-2) + fib(n-1)
 
 if __name__ == "__main__":
 	main()
---
+++ util.py
@@ -0,0 +1,7 @@
+
+def fib(n):
+	"""Find the nth term of the Fibonacci sequence."""
+	if n < 2:
+		return n
+	
+	return fib(n-2) + fib(n-1)
